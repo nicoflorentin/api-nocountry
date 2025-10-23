3e96841e247bb2fd0784b04fcaecd43c3b59a274
Miguel <mickychog@gmail.com>
2025-10-22 23:28:16 -0400

    refactoring Swagger documentation

---
+++ src/docs/doctor.doc.ts
@@ -0,0 +1,194 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Doctor
+ *   description: Operaciones relacionadas con doctores
+ */
+
+
+/**
+ * @swagger
+ * /api/doctor:
+ *   get:
+ *     summary: Obtener lista de doctor
+ *     tags: [Doctor]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Lista de doctor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 doctor:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/DoctorResponse'
+ */
+
+/**
+ * @swagger
+ * /api/doctor/create:
+ *   post:
+ *     summary: Crear usuario doctor
+ *     tags: [Doctor]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DoctorCreate'
+ *     responses:
+ *       201:
+ *         description: Doctor creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DoctorResponse'
+ *       400:
+ *         description: Datos inválidos
+ *       500:
+ *         description: Error interno del servidor
+ */
+
+
+/**
+ * @swagger
+ * /api/doctor/search:
+ *   get:
+ *     summary: Crear usuario doctor
+ *     tags: [Doctor]
+ *     parameters:
+ *       - name: name
+ *         in: query
+ *         required: true
+ *         description: Nombre del doctor
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Doctores obtenidos exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DoctorResponse'
+ *       400:
+ *         description: Datos inválidos
+ *       500:
+ *         description: Error interno del servidor
+ */
+
+
+/**
+ * @swagger
+ * /api/doctor/put:
+ *   put:
+ *     summary: Actualizar usuario doctor
+ *     tags: [Doctor]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DoctorUpdate'
+ *     responses:
+ *       200:
+ *         description: Doctor actualizado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DoctorResponse'
+ *       400:
+ *         description: Datos inválidos
+ *       409:
+ *         description: Algun campo ya existe en la base de datos y no se puede actualizar
+ *       500:
+ *         description: Error interno del servidor
+ */
+
+
+/**
+ * @swagger
+ * /api/doctor/{id}:
+ *   get:
+ *     summary: Obtener médico por ID
+ *     tags: [Doctor]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID del médico
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Detalles del doctor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 doctor:
+ *                   $ref: '#/components/schemas/DoctorResponse'
+ *       404:
+ *         description: Doctor no encontrado
+ *       500:
+ *         description: Error interno del servidor
+ */
+
+
+/**
+ * @swagger
+ * /api/doctor/create:
+ *   post:
+ *     summary: Crear usuario doctor
+ *     tags: [Doctor]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DoctorCreate'
+ *     responses:
+ *       201:
+ *         description: Doctor creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DoctorResponse'
+ *       400:
+ *         description: Datos inválidos
+ *       500:
+ *         description: Error interno del servidor
+ */
+
+
+/**
+ * @swagger
+ * /api/doctor/specialty/{id}:
+ *   get:
+ *     summary: Crear usuario doctor
+ *     tags: [Doctor]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID de la espaecialidad
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Doctores obtenidos exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DoctorResponse'
+ *       400:
+ *         description: Datos inválidos
+ *       500:
+ *         description: Error interno del servidor
+ */
